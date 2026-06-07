f1ff1aad87939bb3685d56e43df240b926efdbdc
Mikael Kolanowski <mikael.kolanowski97@gmail.com>
2026-06-07 18:46:58 +0200

    lexing skeleton WIP

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,28 +1,26 @@
 package lexer
 
 import (
+	"bufio"
 	"io"
+	"unicode"
 
 	"github.com/mikael-kolanowski/abacus/token"
 )
 
-type Position struct {
-	Filename string
-	Line     int
-	Column   int
-}
+var eof = rune(0)
 
 type Lexer struct {
 	line int
 	col  int
-	r    io.Reader
+	r    *bufio.Reader
 }
 
 func NewLexer(r io.Reader) *Lexer {
 	return &Lexer{
 		line: 1,
 		col:  0,
-		r:    r,
+		r:    bufio.NewReader(r),
 	}
 }
 
@@ -30,6 +28,50 @@ func (l *Lexer) currentPos() token.Position {
 	return token.Position{"unknown", l.line, l.col}
 }
 
-func (l *Lexer) Next() (token.Position, token.Token, string) {
-	return l.currentPos(), token.Eof, ""
+func (l *Lexer) Next() (token.Token, string) {
+	ch := l.read()
+
+	unicode.IsLetter()
+	if unicode.IsSpace(ch) {
+		l.unread()
+		// TODO scan whitespace
+	}
+
+	// TODO: handle newlines
+
+	// TODO scan number
+
+	// TODO: scan identifier
+
+	// scan individual characters
+	switch ch {
+	case eof:
+		return token.Eof, ""
+	case '*':
+		return token.Mul, string(ch)
+	}
+	return token.Eof, ""
+}
+
+func (l *Lexer) scanWhitespace() {
+	for {
+		if ch := l.read(); ch != eof {
+			break
+		} else if !unicode.IsSpace(ch) {
+			l.unread()
+			break
+		}
+	}
+}
+
+func (l *Lexer) read() rune {
+	ch, _, err := l.r.ReadRune()
+	if err != nil {
+		return eof
+	}
+	return ch
+}
+
+func (l *Lexer) unread() {
+	l.r.UnreadRune()
 }
